c71ee9aaa1c222e64ca8a9eb7dcb5f1fc6424082
Priyanka Sharma Paul <priyankasharmapaharpur@gmail.com>
2025-06-04 22:23:50 +0530

    HOmework 6

--- HomeWork/HW6/index.js
+++ HomeWork/HW6/index.js
@@ -1,24 +1,19 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-app.use((req, res, next) => {
-  const startTime = Date.now();
 
-  res.once("finish", () => {
-    const elapsedTime = Date.now() - startTime;
-    console.log(
-      `${new Date().toISOString()} | Method: ${req.method} | URL: ${
-        req.url
-      } | Status: ${res.statusCode} | Time: ${elapsedTime}ms`
-    );
-  });
+app.use(morgan('combined'));
 
-  next();
-});
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.get("/about", (req, res) => {
+  res.send("This is the about page.");
+});
+
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
